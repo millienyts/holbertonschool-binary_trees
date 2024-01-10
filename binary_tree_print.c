e885ad594f24ea0723abd83d894ac1e8ea730eff
millienytorressanta <tmillieny@gmail.com>
2024-01-10 19:42:35 -0400

    testing

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -5,16 +5,16 @@
 /* Function to print a binary tree */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+	return;
 
-    printf("%d\n", tree->n);
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        printf("Left ");
-        binary_tree_print(tree->left);
-        printf("Right ");
-        binary_tree_print(tree->right);
-    }
+	printf("%d\n", tree->n);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+	printf("Left ");
+	binary_tree_print(tree->left);
+	printf("Right ");
+	binary_tree_print(tree->right);
+	}
 }
 
