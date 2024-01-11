117a367db219c07971ed1a3ff8fe02abd3575595
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-11 12:09:55 -0400

    new node

--- binary_trees.h
+++ binary_trees.h
@@ -1,21 +1,21 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
-
-/* Structure for a basic binary tree node */
+/* Basic Binary Tree Structure */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
-/* Function prototypes */
+/* Function to print binary trees (provided for visualization) */
+void binary_tree_print(const binary_tree_t *);
+
+/* Function Prototype for Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
