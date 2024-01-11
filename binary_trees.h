61ae740dda9d74947d79b79e157bfdbefe2e8a0e
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-11 13:51:43 -0400

    insert left

--- binary_trees.h
+++ binary_trees.h
@@ -12,10 +12,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-/* Function to print binary trees (provided for visualization) */
 void binary_tree_print(const binary_tree_t *);
-
-/* Function Prototype for Task 0 */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
